4f3a685006c5897629b7d5ab746c283e8cdc552f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-12 10:35:27 +0000

    Deploy mrKrabe/img2sound to github.com/mrKrabe/img2sound.git:gh-pages

---
+++ precache-manifest.4cfb6d214cde53e212e90d25c060bd89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "835d638740a60331cdb8",
+    "url": "/img2sound/static/css/main.48e79466.chunk.css"
+  },
+  {
+    "revision": "835d638740a60331cdb8",
+    "url": "/img2sound/static/js/main.835d6387.chunk.js"
+  },
+  {
+    "revision": "64a53fbfe4d2f3f4784a",
+    "url": "/img2sound/static/js/1.64a53fbf.chunk.js"
+  },
+  {
+    "revision": "64816785006b15bcbbc8",
+    "url": "/img2sound/static/js/runtime~main.64816785.js"
+  },
+  {
+    "revision": "09a430cdb70ba80d36cad084860ab446",
+    "url": "/img2sound/index.html"
+  }
+];
